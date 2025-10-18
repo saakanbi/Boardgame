22a07c818d64e5f3dcfec731ee88a9373e7c413a
saakanbi <woakanbi@gmail.com>
2025-10-18 15:34:57 -0600

    fixed

--- src/main/java/com/javaproject/beans/ErrorMessage.java
+++ src/main/java/com/javaproject/beans/ErrorMessage.java
@@ -1,12 +1,22 @@
 package com.javaproject.beans;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class ErrorMessage {
     private final String STATUS = "error";
-
     private String message;
+
+    public ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSTATUS() {
+        return STATUS;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
--- src/main/java/com/javaproject/beans/Review.java
+++ src/main/java/com/javaproject/beans/Review.java
@@ -1,11 +1,32 @@
 package com.javaproject.beans;
 
-import lombok.Data;
-
-@Data
 public class Review {
 
     private Long id;
     private Long gameId;
     private String text;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(Long gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
